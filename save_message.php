e1ee43315e91a240659d7aac941e52dec8afdc17
FelixE11 <134155347+FelixE11@users.noreply.github.com>
2023-05-20 20:40:06 +0200

    Update save_message.php

--- save_message.php
+++ save_message.php
@@ -1,29 +1,18 @@
 <?php
-function saveMessage($username, $message, $image) {
-  $file = 'messages.json';
-  $data = [
-    'username' => $username,
-    'message' => $message,
-    'timestamp' => time()
-  ];
-
-  if ($image && $image['error'] === UPLOAD_ERR_OK) {
-    $imageData = file_get_contents($image['tmp_name']);
-    $data['image'] = base64_encode($imageData);
-  }
-
-  $messages = [];
-  if (file_exists($file)) {
-    $messages = json_decode(file_get_contents($file), true);
-  }
-  $messages[] = $data;
-  file_put_contents($file, json_encode($messages));
-}
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $username = $_POST['username'];
   $message = $_POST['message'];
   $image = $_FILES['image'];
-  saveMessage($username, $message, $image);
-}
+
+  // Zapisanie wiadomości i obrazu na serwerze
+
+  // Przykładowe zapisanie wiadomości w pliku tekstowym
+  $file = fopen('messages.txt', 'a');
+  fwrite($file, $message . PHP_EOL);
+  fclose($file);
+
+  // Przykładowe zapisanie obrazu jako pliku na serwerze
+  if ($image) {
+    $imageData = file_get_contents($image['tmp_name']);
+    $imageName = $image['name'];
+    file_put_contents('images/' . $imageName, $imageData);
+  }
 ?>
