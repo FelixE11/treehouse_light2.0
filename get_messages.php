16b1714fbc17d581a3ca88256a176b960cd72c48
FelixE11 <134155347+FelixE11@users.noreply.github.com>
2023-05-20 20:39:49 +0200

    Update get_messages.php

--- get_messages.php
+++ get_messages.php
@@ -1,14 +1,19 @@
 <?php
-function getMessages() {
-  $file = 'messages.json';
-  if (file_exists($file)) {
-    $messages = json_decode(file_get_contents($file), true);
-    return $messages;
-  }
-  return [];
-}
+  // Pobranie wiadomości z serwera
 
-$messages = getMessages();
-header('Content-Type: application/json');
-echo json_encode($messages);
+  // Przykładowe odczytanie wiadomości z pliku tekstowego
+  $messages = file('messages.txt', FILE_IGNORE_NEW_LINES);
+
+  // Przygotowanie danych w formacie JSON
+  $data = array();
+  foreach ($messages as $message) {
+    $data[] = array(
+      'username' => 'Nazwa Użytkownika',
+      'timestamp' => time(),
+      'message' => $message,
+      'image' => null
+    );
+  }
+
+  echo json_encode($data);
 ?>
