7d47dc83e11bbf4b3e73cc9c4f09d6b7c7b42eb9
FelixE11 <134155347+FelixE11@users.noreply.github.com>
2023-05-20 20:59:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,95 +1,110 @@
-$(document).ready(function() {
-  var loginCode = generateLoginCode(); // Wygenerowanie losowego kodu logowania
+var loginCode;
+var username;
 
-  // Wyświetlanie formularza logowania
-  $('#login-code').val(loginCode);
-  $('#login-container').show();
+function login() {
+  loginCode = generateLoginCode();
+  username = loginCode;
 
-  // Obsługa logowania
-  $('#login-button').click(function() {
-    var enteredCode = $('#login-code').val();
-    if (enteredCode === loginCode) {
-      $('#login-container').hide();
-      $('#chat-container').show();
-      loadMessages();
-    } else {
-      alert('Logowanie się nie powiodło. Wprowadź poprawny kod logowania.');
-    }
+  var loginContainer = document.getElementById('login-container');
+  var chatContainer = document.getElementById('chat-container');
+  loginContainer.style.display = 'none';
+  chatContainer.style.display = 'block';
+
+  loadChat();
+}
+
+function generateLoginCode() {
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  var code = '';
+  for (var i = 0; i < 5; i++) {
+    code += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return code;
+}
+
+function loadChat() {
+  var messageContainer = document.getElementById('message-container');
+  messageContainer.innerHTML = '';
+
+  // TODO: Pobierz wcześniejsze wiadomości z serwera i wyświetl je w porządku od najnowszych do najstarszych
+
+  // Przykładowe wiadomości
+  var messages = [
+    { username: 'User1', timestamp: new Date(), message: 'Hello!', image: null },
+    { username: 'User2', timestamp: new Date(), message: 'Hi!', image: null }
+  ];
+
+  messages.sort(function(a, b) {
+    return b.timestamp - a.timestamp;
   });
 
-  // Obsługa wysyłania wiadomości
-  $('#send-button').click(function() {
-    var message = $('#message-input').val();
-    var image = $('#image-input')[0].files[0];
-
-    if (message !== '' || image) {
-      sendMessage(message, image);
-      $('#message-input').val('');
-      $('#image-input').val('');
-    }
+  messages.forEach(function(message) {
+    var timestamp = formatDate(message.timestamp);
+    var messageElement = createMessageElement(message.username, timestamp, message.message, message.image);
+    messageContainer.appendChild(messageElement);
   });
 
-  // Generowanie losowego kodu logowania
-  function generateLoginCode() {
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    var code = '';
-    for (var i = 0; i < 5; i++) {
-      code += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return code;
-  }
+  // Przewiń do ostatniej wiadomości
+  messageContainer.scrollTop = messageContainer.scrollHeight;
+}
 
-  // Ładowanie wiadomości z serwera
-  function loadMessages() {
-    $.ajax({
-      url: 'get_messages.php',
-      type: 'GET',
-      dataType: 'json',
-      success: function(data) {
-        $('#messages-container').empty();
-        for (var i = data.length - 1; i >= 0; i--) {
-          var message = data[i];
-          var timestamp = formatTimestamp(message.timestamp);
-          var html = '<div class="message">' +
-                       '<div class="message-header">' +
-                         '<span class="username">' + message.username + '</span>' +
-                         '<span class="timestamp">' + timestamp + '</span>' +
-                       '</div>' +
-                       '<div class="message-body">' +
-                         '<p>' + message.message + '</p>';
-          if (message.image) {
-            html += '<img src="data:image/jpeg;base64,' + message.image + '" />';
-          }
-          html += '</div></div>';
-          $('#messages-container').append(html);
-        }
-      }
-    });
-  }
+function createMessageElement(username, timestamp, message, image) {
+  var messageElement = document.createElement('div');
+  messageElement.className = 'message';
 
-  // Wysyłanie wiadomości na serwer
-  function sendMessage(message, image) {
-    var formData = new FormData();
-    formData.append('message', message);
-    formData.append('image', image);
+  var usernameElement = document.createElement('span');
+  usernameElement.className = 'username';
+  usernameElement.textContent = username;
 
-    $.ajax({
-      url: 'save_message.php',
-      type: 'POST',
-      data: formData,
-      contentType: false,
-      processData: false,
-      success: function() {
-        loadMessages();
-      }
-    });
-  }
+  var timestampElement = document.createElement('span');
+  timestampElement.className = 'timestamp';
+  timestampElement.textContent = timestamp;
 
-  // Formatowanie znacznika czasu
-  function formatTimestamp(timestamp) {
-    var date = new Date(timestamp);
-    var options = { day: 'numeric', month: 'numeric', year: 'numeric', hour: '2-digit', minute: '2-digit' };
-    return date.toLocaleDateString('pl-PL', options);
-  }
-});
+  var messageTextElement = document.createElement('p');
+  messageTextElement.textContent = message;
+
+  messageElement.appendChild(usernameElement);
+  messageElement.appendChild(document.createElement('br'));
+  messageElement.appendChild(timestampElement);
+  messageElement.appendChild(document.createElement('br'));
+  messageElement.appendChild(messageTextElement);
+
+  return messageElement;
+}
+
+function formatDate(timestamp) {
+  var date = new Date(timestamp);
+  var options = { day: 'numeric', month: 'numeric', year: 'numeric', hour: '2-digit', minute: '2-digit' };
+  return date.toLocaleDateString('pl-PL', options);
+}
+
+function sendMessage() {
+  var messageInput = document.getElementById('message-input');
+  var imageInput = document.getElementById('image-input');
+
+  var message = messageInput.value;
+  var image = imageInput.files[0];
+
+  // TODO: Wyślij wiadomość na serwer
+
+  // Przykładowe wysłanie wiadomości
+  var newMessage = {
+    username: username,
+    timestamp: new Date(),
+    message: message,
+    image: image
+  };
+
+  var messageContainer = document.getElementById('message-container');
+  var messageElement = createMessageElement(newMessage.username, formatDate(newMessage.timestamp), newMessage.message, newMessage.image);
+  messageContainer.appendChild(messageElement);
+
+  // Przewiń do ostatniej wiadomości
+  messageContainer.scrollTop = messageContainer.scrollHeight;
+
+  // Wyczyść pola wprowadzania
+  messageInput.value = '';
+  imageInput.value = '';
+}
+
 
